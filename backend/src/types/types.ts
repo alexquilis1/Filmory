8d82db2cf9f1905747a14b12ef0e5488c969b492
Alex Quilis Vila <alexquilis1@gmail.com>
2024-12-12 21:01:19 +0100

    Remove all files from the repository (starting fresg)

--- backend/src/types/types.ts
+++
@@ -1,6 +0,0 @@
-// types/express.d.ts
-declare namespace Express {
-    export interface Request {
-        user?: { user_id: number };  // Add userId in the user property
-    }
-}
--- backend/src/types/watchlistTypes.ts
+++
@@ -1,8 +0,0 @@
-export interface WatchlistItem {
-    id: number;
-    type: 'movie' | 'tv';
-    tmdb_id: number;
-    added_date: string;
-    watched: boolean;
-    user_id: number;
-}
